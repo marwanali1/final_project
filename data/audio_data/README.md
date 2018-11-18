476ee2914804fe71318bdc3cc52ac4227b473ab3
Marwan Ali <marwanali2428@gmail.com>
2018-11-18 16:08:30 -0500

    Created training and test datasets from original dataset

---
+++ data/audio_data/README.md
@@ -0,0 +1,22 @@
+# Dataset Info
+
+The original dataset has 59600 total.
+https://labrosa.ee.columbia.edu/millionsong/blog/11-2-28-deriving-genre-dataset
+
+**Number of songs per genre:**  
+classic pop and rock: 23895  
+punk: 3200  
+folk: 13192  
+pop: 1617  
+dance and electronica: 4935  
+metal: 2103  
+jazz and blues: 4334  
+classical: 1874  
+hip-hop: 434  
+soul and reggae: 4016  
+
+**Training Dataset**  
+Of the original 59600, 30% of each genre will be used to create a training dataset.
+
+**Test Dataset**  
+The remaining 70% of the dataset will be used to as the test dataset.
--- src/genre_predictor.py
+++ src/genre_predictor.py
@@ -7,18 +7,52 @@ class GenrePredictor:
     def __init__(self):
         self.training_data = None
         self.test_data = None
+        self.__load_datasets()
 
-        self.__parse_training_data()
-        self.__parse_test_data()
+    @staticmethod
+    def create_datasets():
+        """
+        Used for creating training and test datasets from the original dataset
+        """
+        # Load the data set in the csv to a Pandas data frame
+        msd_genre_df = pd.read_csv(filepath_or_buffer='data/audio_data/msd_genre_dataset.csv')
 
-    def __clean_line(self, line):
-        pass
+        training_set = None
+        test_set = None
+        for genre in msd_genre_df.genre.unique():
+            genre_df = msd_genre_df.loc[msd_genre_df['genre'] == genre]
+            training_sample = genre_df.sample(frac=0.25)
+            genre_training_set = training_sample[
+                ['genre', 'track_id', 'loudness', 'tempo', 'time_signature', 'key', 'mode', 'duration', 'avg_timbre1',
+                 'avg_timbre2', 'avg_timbre3', 'avg_timbre4', 'avg_timbre5', 'avg_timbre6', 'avg_timbre7',
+                 'avg_timbre8', 'avg_timbre9', 'avg_timbre10', 'avg_timbre11', 'avg_timbre12', 'var_timbre1',
+                 'var_timbre2', 'var_timbre3', 'var_timbre4', 'var_timbre5', 'var_timbre6', 'var_timbre7',
+                 'var_timbre8', 'var_timbre9', 'var_timbre10', 'var_timbre11', 'var_timbre12']]
 
-    def __parse_training_data(self):
-        pass
+            if training_set is None:
+                training_set = genre_training_set
+            else:
+                training_set = pd.concat([training_set, genre_training_set])
 
-    def __parse_test_data(self):
-        pass
+            test_sample = pd.merge(genre_df, training_sample, indicator=True, how='outer').query('_merge=="left_only"').drop('_merge', axis=1)
+            genre_test_set = test_sample[
+                ['track_id', 'loudness', 'tempo', 'time_signature', 'key', 'mode', 'duration', 'avg_timbre1', 'avg_timbre2',
+                 'avg_timbre3', 'avg_timbre4', 'avg_timbre5', 'avg_timbre6', 'avg_timbre7', 'avg_timbre8', 'avg_timbre9',
+                 'avg_timbre10', 'avg_timbre11', 'avg_timbre12', 'var_timbre1', 'var_timbre2', 'var_timbre3', 'var_timbre4',
+                 'var_timbre5', 'var_timbre6', 'var_timbre7', 'var_timbre8', 'var_timbre9', 'var_timbre10', 'var_timbre11',
+                 'var_timbre12']]
+
+            if test_set is None:
+                test_set = genre_test_set
+            else:
+                test_set = pd.concat([test_set, genre_test_set])
+
+        training_set.to_csv(path_or_buf='data/audio_data/training_data.csv', index=False)
+        test_set.to_csv(path_or_buf='data/audio_data/test_data.csv', index=False)
+
+    def __load_datasets(self):
+        self.training_data = pd.read_csv(filepath_or_buffer='data/audio_data/training_data.csv')
+        self.test_data = pd.read_csv(filepath_or_buffer='data/audio_data/test_data.csv')
 
     def cross_validate(self):
         pass
@@ -31,18 +65,8 @@ if __name__ == "__main__":
     print("START")
     print(datetime.datetime.time(datetime.datetime.now()))
 
-    # Load the data set in the csv to a Pandas data frame
-    msd_genre_df = pd.read_csv('data/msd_genre_dataset.csv')
-
-    # Print all the columns in the data set
-    col_names = list(msd_genre_df.columns.values)
-    print(col_names)
-
-    # Print the first row of the data set
-    print(msd_genre_df.iloc[0])
-
-    # predictor = GenrePredictor()
-    # predictor.predict()
+    predictor = GenrePredictor()
+    predictor.predict()
     # predictor.cross_validate()
 
     print('\nFINISH')
